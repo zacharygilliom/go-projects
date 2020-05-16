4e56e321d203592d1932b3e8f91408bd9ab34eb7
Zachary Gilliom <zacharygilliom@gmail.com>
2020-05-16 19:52:23 -0400

    updated mathpractice.go file

--- basic-math/mathpractice.go
+++ basic-math/mathpractice.go
@@ -8,18 +8,25 @@ func add(a float64, b float64) float64 {
 	return a + b
 }
 
-func multiply(c float64, d float64, e float64, f float64) float64 {
-	num1 := add(c, d)
-	num2 := add(e, f)
-	product := num1 * num2
-	return product
+func divide(g float64, h float64) float64 {
+	return g / h
+}
 
+func multiply(c float64, d float64) float64 {
+	return c * d
+}
+
+func subtract(x float64, y float64) float64 {
+	return x - y
 }
 
 func main() {
-	var num1 float64 = 5
-	var num2 float64 = 10
-	var num3 float64 = 2
-	var num4 float64 = 11
-	fmt.Println(multiply(num1, num2, num3, num4))
+	var x1 float64 = 12.2
+	var x2 float64 = 29.0
+	k := add(x1, x2)
+	j := subtract(x2, x1)
+	m := multiply(k, j)
+	n := divide(j, k)
+	answer := multiply(m, n)
+	fmt.Println(answer)
 }
