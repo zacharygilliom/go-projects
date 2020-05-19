b6b309c40c90c10915e0a3c4ca25e77e78894aaf
Zachary Gilliom <zacharygilliom@gmail.com>
2020-05-18 21:46:13 -0400

    Updated main file with credential builder.

--- spotify-api/main.go
+++ spotify-api/main.go
@@ -3,22 +3,38 @@ package main
 import (
 	"fmt"
 	/*"io/ioutil"*/
-	"net/http"
+	b64 "encoding/base64"
+	//"net/http"
 )
 
 func main() {
-
-	client_id, client_secret := getclientinfo()
-	url := "https://accounts.spotify.com/authorization?client_id=" + client_id
-	resp, err := http.Get(url)
-	if err != nil {
-		fmt.Println("Invalid client_id passed: %d", err)
-	}
-	fmt.Println(resp)
-	fmt.Println(client_secret)
+	encodedString := encodeIdAndSecret()
+	fmt.Println(encodedString)
 }
 
-func getclientinfo() (string, string) {
+func getClientInfo() (string, string) {
 	client_id, client_secret := client_info()
 	return client_id, client_secret
+}
+
+/*func authUser() {
+
+	client_id, client_secret := getclientinfo()
+	url := "https://accounts.spotify.com/authorize?client_id=" + client_id + "response_type=code&scope=playlist-read-private"
+	resp, err := http.Get(url)
+	if err != nil {
+		fmt.Println(client_secret)
+		fmt.Println("Invalid client_id passed: %d", err)
+	} else {
+		fmt.Println("Authorization Successful")
+		fmt.Println(resp)
+
+	}
+}
+*/
+func encodeIdAndSecret() string {
+	client_id, client_secret := getClientInfo()
+	data := client_id + ":" + client_secret
+	encString := b64.StdEncoding.EncodeToString([]byte(data))
+	return encString
 }
